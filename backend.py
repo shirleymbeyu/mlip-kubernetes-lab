75641da2334ef91f3377812c10ca9098013a8d71
shirleymbeyu <mbeyushirley@gmail.com>
2026-04-22 17:47:28 +0200

    Lab draft

--- backend.py
+++ backend.py
@@ -67,7 +67,8 @@ def predict_engagement():
         # Create feature vector
         features = pd.DataFrame([user_data])[feature_names]
         
-        # TODO: implement prediction of engagement_score using the current_model
+        # Predict engagement_score using the loaded model
+        engagement_score = float(current_model.predict(features)[0])
 
         return jsonify({
             "engagement_score": engagement_score,
